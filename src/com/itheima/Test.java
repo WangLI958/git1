b31a581c2f58e21a6bdf08d972d60e3f592aa48c
hewenzhang <hewenzhang@qq.com>
2019-01-24 22:49:52 +0800

    22

--- src/com/itheima/Test.java
+++ src/com/itheima/Test.java
@@ -3,5 +3,12 @@ package com.itheima;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("Hello Git");
+        System.out.println("HELLO GIT");
+    }
+    public void test(){
+        int i = 10;
+        i += 10;
+        System.out.println(i);
     }
 }
